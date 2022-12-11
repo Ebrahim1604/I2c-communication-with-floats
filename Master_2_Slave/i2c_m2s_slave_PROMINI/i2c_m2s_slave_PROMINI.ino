88c20d27d87281c01f49ebef7c00ad6b85077a7f
Ebrahim1604 <42499258+Ebrahim1604@users.noreply.github.com>
2022-12-11 06:09:40 +0530

    Update i2c_m2s_slave_PROMINI.ino

--- Master_2_Slave/i2c_m2s_slave_PROMINI/i2c_m2s_slave_PROMINI.ino
+++ Master_2_Slave/i2c_m2s_slave_PROMINI/i2c_m2s_slave_PROMINI.ino
@@ -1,282 +1,126 @@
-#include <arduino.h>
 #include <Wire.h>
+#include <string.h>
 #include "slave_helper.h"
-
-//______________________________
-//char key[] = {'K','E','Y','1','2','3','\0'};
-char key[] = "KEY123";
-#define key_len 7
-
-#define rx_timeout 500 //in milli sec
-
 /*
- * i2c Addressing will be as follows:
- * Master: 8
- * Slaves: 9 to 119, depending on the number of slaves connected
+
+/* Note:
+ * sen_values is the array which stores the number of variables
+ * sen_values[0] is always the number of sensor values sent by 
+ * the master. sensor values are stored from index 1 to 7, depending
+ * on the number of values sent.
  */
- 
-#define I2C_MASTER_ADDR 8
-#define I2C_MY_ADDR 9
-//_________________________________
 
-int master_response,auth_result;
+double* sen_values;
+static double datatomaster[8];
+void update_datatomaster(void);
 
-//static double data2master[8];
+int incoming_byte = 0;
+int request_from_master = 0;
 
-void default_sensor_values(void)
-{
-    for (int i=0; i<8; i++)
-    {
-      sensor_values[i] = 0.0;
-      }
-  }
+bool sensor_val_updated_ = 0;
 
-void begin_i2c(void)
-{
-  Wire.begin(I2C_MY_ADDR); // join i2c bus
-  }
+void setup() {
+  // put your setup code here, to run once:
+
+  Serial.begin(9600);        // start serial for output
   
-double byte2float(int b1, int b2, int b3, int b4)
-{
-  union b2f {
+  default_sensor_values();
   
-    char _buffer[4];
-    double voltageReading;
+  begin_i2c();
   
-  } converter;
+  Wire.onReceive(receiveEvent); // register receive event
+  Wire.onRequest(requestEvent); // register request event
 
-  /*
-  Serial.println(b1);
-  Serial.println(b2);
-  Serial.println(b3);
-  Serial.println(b4);
-  */
-  
-  converter._buffer[0] = b1;
-  converter._buffer[1] = b2;
-  converter._buffer[2] = b3;
-  converter._buffer[3] = b4;
-
-  return converter.voltageReading;
-  
+  Serial.println("Begining the slave process...!");
 }
 
-int* float2bytes(double sensor_val)
-{
-    union floatToBytes {
+void loop() {
+  // put your main code here, to run repeatedly:
   
-    char _buffer[4];
-    double voltageReading;
-  
-  } converter;
-
-  converter.voltageReading = sensor_val;
-
-  static int f2b[4];
-
-  f2b[0] = converter._buffer[0];
-  f2b[1] = converter._buffer[1];
-  f2b[2] = converter._buffer[2];
-  f2b[3] = converter._buffer[3];
-
-  Serial.println(f2b[0]);
-  Serial.println(f2b[1]);
-  Serial.println(f2b[2]);
-  Serial.println(f2b[3]);
-  
-  return f2b;  
-  }
-  
-double* get_latest_values(void)
-{
-  static double sen_vals[8];
-
-  sen_vals[0] = (double)updated_values[0]/4.0;
-
-  for (int i=1; i<(int)sen_vals[0]+1; i++)
-  {
-       sen_vals[i] = byte2float(updated_values[4*(i-1)+ 1],updated_values[4*(i-1)+ 2],updated_values[4*(i-1)+ 3],updated_values[4*(i-1)+ 4]);
-    }
-
-    return sen_vals;
-  }
-
-void i2c_send(int master_addr, int* _data, int data_len)
-{
-  
-   Wire.beginTransmission(master_addr);
-
-   Serial.print("sending Following data: ");
-   for(int i=0; i<data_len; i++)
-   {
-      Wire.write(_data[i]);
-      Serial.print(_data[i]);
-      Serial.print(" ");
-    }
-
-    Serial.println("");
-   
-   int error = Wire.endTransmission();
-
-   if(error == 0)
-   {
-      Serial.println("Response sent successfully to master ....");
-    }
-
-   else if(error == 1)
-   {
-      Serial.println("Bufferoverloaded, please restrict the data length to less than 32 bytes");
-    }
-
-   else if(error == 5)
-   {
-      Serial.println("Timeout, No response from SLave");
-   }
-
-   else
-   {
-      Serial.println("Error in communication. Check connection or Master device");
-    }
-  }
-
-void handle_data_request(double* data2master)
-{
-  if(Wire.read() == 0x06)
-  {
-    Serial.println("Master Data request acknowledged!");
-    }
-
-    int no_of_values = (int)data2master[0];
-   
-   static int s2m[32];
-
-   s2m[0] = 0x01;
-   s2m[1] = 0x05;
-   s2m[2] = 4*no_of_values;
-
-   Serial.println("Following data is sent to master: ");
-   for (int i=1; i<(no_of_values+1); i++)
-   {
-    int* _float2byte = float2bytes(data2master[i]);
-    
-    for(int j=0; j<4; j++)
+  if(sensor_val_updated_ == 1)
     {
-      s2m[3+(4*(i-1))+j] = _float2byte[j];
-      Serial.print(_float2byte[j]);
-      Serial.print(" ");
-    }
-    Serial.println("");
-   }
-   
-
-   //noInterrupts();
-   
-   //i2c_send(I2C_MASTER_ADDR,s2m,(4*no_of_values+3));
-   i2c_send(I2C_MASTER_ADDR,s2m,32);
-   //master_response = 0;
-   Serial.println("Data sent to master!");
-
-   //master_response = 2;
-   }
-  
-void handle_data_sending(void)
-{
-  int noofvals = Wire.read();
-  updated_values[0] = noofvals;
-  //Serial.println("Following data has been received");
-  for (int i=1; i<(int)(noofvals+1); i++)
-  {
-    int temp_val = Wire.read();
-    if(temp_val >127)
-    {
-      temp_val = 256 - temp_val;
-      temp_val *= -1;
+      //noInterrupts();
+      sen_values = get_latest_values();
+      Serial.print("Following ");
+      Serial.print((int)sen_values[0]);
+      Serial.println(" Sensor values have been reveived: ");
+      
+      for(int i=1; i<(int)(sen_values[0]+1); i++)
+      {
+        Serial.print("Val");
+        Serial.print(i);
+        Serial.print(" = ");
+        Serial.println(sen_values[i]);
+        }
+      sensor_val_updated_ = 0;
       }
-    updated_values[i] = temp_val;
-    //Serial.print(updated_values[i]);
-    //Serial.print(" ");
-    }
-    //Serial.println("");
-    //if req respond to master
-  }
-  
-void handle_auth(void)
-{
-  
-  int temp_key_len = Wire.read();
-  Serial.print("Length of key received = ");
-  Serial.println(temp_key_len);
-  char temp_key[temp_key_len];
+  //sen_values[1],sen_values[2]... can be used in your code further
 
-  Serial.print("Following Characters received: ");
-  for(int i=0; i<temp_key_len; i++)
+  if (incoming_byte == 1)
   {
-    temp_key[i] = Wire.read();
-    Serial.print(temp_key[i]);
+    Serial.println("Incoming Values from i2c Bus, responding to the same...");
+
+    if (Wire.read() == 0x02)
+        {
+          int second_byte = Wire.read();
+
+          if(second_byte == 0x03)
+            {
+              Serial.println("Request for Authentication received....");
+              handle_auth();
+              }
+           else if(second_byte == 0x04)
+            {
+              Serial.println("Receiving data from master....");
+              handle_data_sending();
+              sensor_val_updated_ = 1;
+              }
+           else if(second_byte == 0x05)
+           {
+              Serial.println("Request for Data received....");
+              handle_data_request(datatomaster);
+            }
+          }
+    incoming_byte = 0;
     }
-  Serial.println("");
-  int result = strcmp(temp_key,key);
 
-  if(result == 0)
-  {
-    Serial.println("Key matched! Responding to master..... ");
-    }
-  else
-  {
-    Serial.println("Key did not match! Please check the key[] and key_len variable values in master and slave");
-    }
-    
-  master_response = 1;
-  auth_result = result;
-  }
-
-void respond_to_master(double* data2master)
-{
-  if (master_response == 1) //response for authentication
-  { 
-    static int auth_response[3];
-
-    auth_response[0] = 0x01;
-    auth_response[1] = 0x03;
-    auth_response[2] = !auth_result;
-
-    i2c_send(I2C_MASTER_ADDR,auth_response,3);
-    master_response = 0;
-    return;
-   }
-  /*
-  else if(master_response == 2)
-  {
-    int no_of_values = (int)data2master[0];
-   
-   static int s2m[32];
-
-   s2m[0] = 0x01;
-   s2m[1] = 0x05;
-   s2m[2] = 4*no_of_values;
-
-   Serial.println("Following data is sent to master: ");
-   for (int i=1; i<(no_of_values+1); i++)
+   if(request_from_master == 1)
    {
-    int* _float2byte = float2bytes(data2master[i]);
-    
-    for(int j=0; j<4; j++)
-    {
-      s2m[3+(4*(i-1))+j] = _float2byte[j];
-      Serial.print(_float2byte[j]);
-      Serial.print(" ");
+      Serial.println("MAster requesting authentication response....");
+      respond_to_master(datatomaster);
+      request_from_master = 0;
+      incoming_byte = 0;
+      return;
     }
-    Serial.println("");
-   }
    
+  update_datatomaster(); 
+}
 
-   //noInterrupts();
-   
-   //i2c_send(I2C_MASTER_ADDR,s2m,(4*no_of_values+3));
-   i2c_send(I2C_MASTER_ADDR,s2m,32);
-   master_response = 0;
-   Serial.println("Data sent to master!");
-   return;
-   */
-    //}
+void update_datatomaster(void)
+{
+  int no_of_values = 4;// define the number of sensor values you are sending
+
+    //replace these values actual values
+    //Note: The size of this array is 8 which is already defined globally
+  datatomaster[0] = (double)no_of_values; // the number of sensor readings to be sent to the i2c slave
+  datatomaster[1] = -30.64;
+  datatomaster[2] = 200.04;
+  datatomaster[3] = 150.19;
+  datatomaster[4] = -1.02;
+  
+  }
+
+void receiveEvent(int numBytesReceived)
+{
+   if(Wire.available())
+     {  
+        incoming_byte = 1;
+        return;
+      }
+  }
+
+void requestEvent()
+{
+  request_from_master = 1;
+  return;
   }
